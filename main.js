5e3632ec17557f3a169d7e5c592ba1218675aa1c
Zoha Nafees <zohanafees09@gmail.com>
2024-06-08 00:23:15 +0500

    TS task 9

---
+++ main.js
@@ -0,0 +1,4 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let name = ["Jahanzaib", "Zoha", "Ayan", "Hamdan"];
+console.log(name[0], "\n", name[1], "\n", name[2], "\n", name[3], "\n", name[4]);
+export {};
